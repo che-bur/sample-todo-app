98a0d23ac4232b4ead3a5a39c2b223198862b90a
chebur <chebu@tutamail.com>
2025-02-17 16:34:39 +0300

    BACKEND: Start server using Echo lib

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "log"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-	log.Println("Hello TODO")
+	server := echo.New()
+
+	// Define a JSON response route
+	server.GET("/", func(context echo.Context) error {
+		data := map[string]string{"status": "OK"}
+		return context.JSON(http.StatusOK, data)
+	})
+
+	// Start the server on port 8080
+	server.Logger.Fatal(server.Start(":8080"))
 }
